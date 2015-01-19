5154ca81ffc4cb3b777fb38f20e0cb91e99aed6e
shayba <shayba@aramnik.com>
2015-01-19 11:30:07 -0800

    WIP

--- wear/src/main/java/com/shaibarack/rickrollwatchface/WatchFaceService.java
+++ wear/src/main/java/com/shaibarack/rickrollwatchface/WatchFaceService.java
@@ -1,6 +1,25 @@
 package com.shaibarack.rickrollwatchface;
 
+import android.graphics.Movie;
 import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.view.SurfaceHolder;
 
 public class WatchFaceService extends CanvasWatchFaceService {
+
+    @Override
+    public Engine onCreateEngine() {
+        return new Engine();
+    }
+
+    private class Engine extends CanvasWatchFaceService.Engine {
+
+        private Movie mMovie;
+
+        @Override
+        public void onCreate(SurfaceHolder holder) {
+            super.onCreate(holder);
+
+            //mMovie = Movie.decodeStream(getResources().openRawResource(R));
+        }
+    }
 }
